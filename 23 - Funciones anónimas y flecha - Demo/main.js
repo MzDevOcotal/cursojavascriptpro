1f8c2838e0462b8d037c14922886e6a97e33e49a
MzDevOcotal <mrzapata74@gmail.com>
2022-01-08 14:24:47 -0600

    Más Ejercicios sobre funciones tradicional y de flecha

--- 23 - Funciones anónimas y flecha - Demo/main.js
+++ 23 - Funciones anónimas y flecha - Demo/main.js
@@ -1,11 +1,11 @@
-
+//FUNCION SUMA CON SINTAXIS TRADICIONAL
 /* function suma(n1,n2){
   let resultado = n1 + n2;
   return resultado;
 } */
 
+//FUNCION SUMA CON SINTAXIS DE FLECHA
 const suma = (n1,n2) =>{
-  
   let resultado = 0;
   if (n1<n2) {
     resultado = n1 * n2;
@@ -14,5 +14,28 @@ const suma = (n1,n2) =>{
   }
   return resultado;
 }
-
 document.getElementById('suma').innerHTML=`La sumatoria es: ${suma(5,8)}`;
+
+//NOTACION DE FUNCION FLECHA CUANDO SOLO HAY UN PARÁMETRO | SE PUEDEN ELIMINAR PARÉNTESIS DEL PARÁMETRO
+const saludo = nombre => 'Hola, ' + nombre;
+document.getElementById('saludo').innerHTML = `Bienvenido: ${saludo('Mario Zapata')}`;
+console.log(saludo(' Mario'));
+
+
+/* MÁS EJERCICIOS */
+
+//CONVERTIR GRADOS A KELVIN | SINTAXIS TRADICIONAL
+function gradosCaK(valor){
+  return valor * 274.15;
+}
+document.getElementById('grados').innerHTML = `Los grado C° equivalentes son: ${gradosCaK(5)}`
+
+//CONVERTIR GRADOS A KELVIN | SINTAXIS FLECHAS
+const gradosCaK2 = (valor) => {
+  return valor * 274.15;
+}
+console.log(gradosCaK2(4));
+
+//CONVERTIR GRADOS A KELVIN | SINTAXIS FLECHAS MÁS SIMPLIFICADA
+const gradosCaK3 = valor => valor * 274.15;
+console.log(gradosCaK3(12));
